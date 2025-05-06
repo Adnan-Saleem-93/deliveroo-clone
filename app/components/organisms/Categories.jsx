218ac1b7bc3b51b0618e8f41a8699285e8277e44
Adnan Saleem <adnan13893@gmail.com>
2025-05-06 19:55:40 +0500

    added loading skeleton cards for categories

--- app/components/organisms/Categories.jsx
+++ app/components/organisms/Categories.jsx
@@ -1,18 +1,23 @@
-import {ScrollView, View} from 'react-native'
+import {ActivityIndicator, ScrollView, View} from 'react-native'
 import React from 'react'
 import CategoryCard from '../molecules/Cards/CategoryCard'
 import {useState} from 'react'
 import {useEffect} from 'react'
 import {getCategories} from '../../../sanity-studio/sanity'
+import SkeletonCard from '../molecules/Cards/SkeletonCard'
 
 const Categories = () => {
   const [categories, setCategories] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const getCategoriesAsync = async () => {
     try {
+      setIsLoading(true)
       const result = await getCategories()
       if (result) setCategories(result)
     } catch (error) {
       console.log(error)
+    } finally {
+      setIsLoading(false)
     }
   }
   useEffect(() => {
@@ -26,10 +31,14 @@ const Categories = () => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       <View className="flex-row items-center gap-x-3">
-        {categories.length > 0 &&
-          categories?.map((category) => {
-            return <CategoryCard key={category._id} {...category} />
-          })}
+        {isLoading
+          ? Array.from({length: 5}).map((number) => {
+              return <SkeletonCard key={number} />
+            })
+          : categories.length > 0 &&
+            categories?.map((category) => {
+              return <CategoryCard key={category._id} {...category} />
+            })}
       </View>
     </ScrollView>
   )
