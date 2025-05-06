7c8150d96bacd42732e7527eb4606be498db4667
Adnan Saleem <80086557+Adnan-Saleem-93@users.noreply.github.com>
2025-05-06 19:55:40 +0500

    added loading skeleton cards for categories

---
+++ app/components/molecules/Cards/SkeletonCard.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {View} from 'react-native'
+
+const SkeletonCard = () => {
+  return <View className="bg-slate-300 rounded-lg h-28 w-28 animate-pulse"></View>
+}
+
+export default SkeletonCard
