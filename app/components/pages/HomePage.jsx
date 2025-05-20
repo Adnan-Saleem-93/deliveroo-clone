b58fd570c34013d6b42615099ec7dc7496d3f120
Adnan Saleem <adnan13893@gmail.com>
2025-05-20 23:52:40 +0500

    added sanity api call for getting featured categories

--- app/components/pages/HomePage.jsx
+++ app/components/pages/HomePage.jsx
@@ -1,14 +1,32 @@
 import {View, ScrollView, Platform} from 'react-native'
-import React, {useLayoutEffect} from 'react'
+import React, {useEffect, useLayoutEffect, useState} from 'react'
 import {useNavigation} from '@react-navigation/native'
 import HomeTemplate from '../templates/HomeTemplate'
 import Categories from '../organisms/Categories'
 import OffersNearYou from '../organisms/OffersNearYou'
 import Featured from '../organisms/Featured'
 import TastyDiscounts from '../organisms/TastyDiscounts'
+import {client} from '../../../sanity-studio/sanity'
+import {getFeaturedCategories} from '../../utils/api'
 
 const HomePage = () => {
   const navigation = useNavigation()
+  const [featuredCategories, setFeaturedCategories] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await getFeaturedCategories()
+        setFeaturedCategories(response)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+
+    return () => {
+      setFeaturedCategories([])
+    }
+  }, [])
 
   useLayoutEffect(() => {
     navigation.setOptions({
