8785ff4309ca4e746a3316247d96e80b2050d393
Adnan Saleem <adnan13893@gmail.com>
2025-05-26 02:34:41 +0500

    Refactored DishItem component layout for improved structure and readability, and adjusted padding in RestaurantPage's VirtualizedList for better spacing.

--- app/components/molecules/Cards/DishItem.jsx
+++ app/components/molecules/Cards/DishItem.jsx
@@ -25,47 +25,50 @@ const DishItem = ({_id, imageUrl, price, name, short_description}) => {
   }
 
   return (
-    <View className="flex flex-col justify-between gap-y-6 border border-gray-200 w-full p-4 bg-white">
-      <TouchableOpacity
-        className="flex flex-row justify-between gap-x-6 w-full"
-        onPress={() => setIsPressed(!isPressed)}
-      >
-        <View className="flex flex-col justify-center gap-y-3 w-1/2">
-          <Text className="text-2xl text-[#353535] font-bold">{name}</Text>
-          <Text className="text-xl text-[#9A9A9A] font-semibold line-clamp-2">
-            {short_description}
-          </Text>
-          <Text className="text-xl text-[#9A9A9A] font-semibold line-clamp-2">${price}</Text>
-        </View>
-        <View>
-          <Image progressiveRenderingEnabled src={imageUrl} className="w-28 h-28 rounded-md" />
-        </View>
-      </TouchableOpacity>
-      {isPressed && (
-        <View className="w-full">
-          <View className="flex flex-row gap-x-3 items-center w-full">
-            {/* REMOVE Item from Cart Button */}
-            <TouchableOpacity
-              className="w-10 h-10 flex items-center justify-center rounded-full bg-[#00CCBC]"
-              onPress={removeItem}
-            >
-              <Text className="text-4xl font-extrabold text-white">-</Text>
-            </TouchableOpacity>
-
-            {/* Item Count */}
-            <Text className="text-2xl font-semibold">{itemCount}</Text>
-
-            {/* ADD Item to Cart Button */}
-            <TouchableOpacity
-              className="w-10 h-10 flex items-center justify-center rounded-full bg-[#00CCBC]"
-              onPress={addItem}
-            >
-              <Text className="text-4xl font-extrabold text-white">+</Text>
-            </TouchableOpacity>
+    <TouchableOpacity
+      className="flex flex-col justify-between gap-y-6 border border-gray-200 w-full p-4 bg-white"
+      onPress={() => setIsPressed(!isPressed)}
+    >
+      <View className="flex flex-col justify-between gap-y-4 w-full">
+        <View className="flex flex-row justify-between items-center gap-x-6 w-full">
+          <View className="flex flex-col justify-center gap-y-3 w-1/2">
+            <Text className="text-2xl text-[#353535] font-bold">{name}</Text>
+            <Text className="text-xl text-[#9A9A9A] font-semibold line-clamp-2">
+              {short_description}
+            </Text>
+            <Text className="text-xl text-[#9A9A9A] font-semibold line-clamp-2">${price}</Text>
+          </View>
+          <View>
+            <Image progressiveRenderingEnabled src={imageUrl} className="w-28 h-28 rounded-md" />
           </View>
         </View>
-      )}
-    </View>
+
+        {isPressed && (
+          <View className="w-full">
+            <View className="flex flex-row gap-x-3 items-center w-full">
+              {/* REMOVE Item from Cart Button */}
+              <TouchableOpacity
+                className="w-10 h-10 flex items-center justify-center rounded-full bg-[#00CCBC]"
+                onPress={removeItem}
+              >
+                <Text className="text-4xl font-extrabold text-white">-</Text>
+              </TouchableOpacity>
+
+              {/* Item Count */}
+              <Text className="text-2xl font-semibold">{itemCount}</Text>
+
+              {/* ADD Item to Cart Button */}
+              <TouchableOpacity
+                className="w-10 h-10 flex items-center justify-center rounded-full bg-[#00CCBC]"
+                onPress={addItem}
+              >
+                <Text className="text-4xl font-extrabold text-white">+</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
+    </TouchableOpacity>
   )
 }
 
